ec255237e2b018660cf430edcc8e4cfe696dc482
Sergio Alexandre <sergioalexandre0716@gmail.com>
2024-09-03 18:39:32 -0300

    add: connection pool

--- Backend/src/config/database.js
+++ Backend/src/config/database.js
@@ -5,7 +5,11 @@ export async function connectDb() {
   const mongoUrl = process.env.DB_URL;
 
   try {
-    await mongoose.connect(mongoUrl);
+    await mongoose.connect(mongoUrl, {
+      maxPoolSize: 10,
+      serverSelectionTimeoutMS: 5000,
+      socketTimeoutMS: 40000,
+    });
     console.log("Connected to MongoDB");
   } catch (error) {
     console.log(error.message);
