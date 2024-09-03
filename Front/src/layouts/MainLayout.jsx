5217de018b8453d69d98334ce98414b3ad7a0fc9
Sergio Alexandre <sergioalexandre0716@gmail.com>
2024-09-03 15:16:25 -0300

    add: include missing ui settings

--- Front/src/layouts/MainLayout.jsx
+++ Front/src/layouts/MainLayout.jsx
@@ -1,10 +1,17 @@
 import Header from "@/components/Header";
+import { Link } from "react-router-dom";
 
 const MainLayout = ({ children }) => {
   return (
     <>
       <Header />
-      <div className="m-auto mt-[15%] flex w-[40%] flex-col gap-8 rounded-xl bg-neutral-900 p-4 text-center text-neutral-200">
+      <div className="m-auto mt-[15%] flex w-[40%] flex-col gap-8 rounded-xl bg-neutral-900 p-4 pt-4 text-center text-2xl text-neutral-200">
+        <div className="flex w-full items-center justify-between pb-4 text-neutral-200">
+          <h2 className="text-xl font-bold">Hello. Guest</h2>
+          <Link to={"/signin"} className="text-xl font-bold">
+            SignOut
+          </Link>
+        </div>
         {children}
       </div>
     </>
