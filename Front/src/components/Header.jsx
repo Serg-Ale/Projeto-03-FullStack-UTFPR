af721198493becc56a5ec721ac4ec9406cfcd84e
Sergio Alexandre <sergioalexandre0716@gmail.com>
2024-09-03 15:16:01 -0300

    add: hover effect to header links

--- Front/src/components/Header.jsx
+++ Front/src/components/Header.jsx
@@ -3,14 +3,20 @@ import Button from "./Button";
 
 const Header = () => {
   return (
-    <header className="flex items-center justify-between bg-neutral-900 p-6">
+    <header className="flex items-center justify-between bg-neutral-900 p-6 font-bold">
       <h1 className="m-auto text-4xl font-bold text-neutral-200">
         Rick and Morty API Search
       </h1>
-      <div className="flex flex-row gap-4">
-        <Link to={"/signin"}>Signin</Link>
-        <Link to={"/list"}>List</Link>
-        <Link to={"/character"}>Create a Character</Link>
+      <div className="flex flex-row gap-8 text-2xl">
+        <Link to={"/signin"} className="hover:text-primary">
+          Signin
+        </Link>
+        <Link to={"/list"} className="hover:text-primary">
+          List
+        </Link>
+        <Link to={"/character"} className="hover:text-primary">
+          Create
+        </Link>
       </div>
     </header>
   );
